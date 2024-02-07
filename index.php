8648f936f078cedc17f0c2d622ac04d0f20ff8c9
Eduardo Portella <eportella70@gmail.com>
2024-02-07 19:11:48 -0300

    aula 045

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-use sistema\Nucleo\Mensagem;
-
 require_once 'sistema/configuracao.php';
 include_once 'sistema/Nucleo/Helpers.php';
 include 'sistema/Nucleo/Mensagem.php';
+use sistema\Nucleo\Helpers;
+
 
 echo Helpers::dataAtual();
 
--- sistema/Nucleo/Helpers.php
+++ sistema/Nucleo/Helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace sistema\Nucleo;
+
 class Helpers {
 
    public static function  dataAtual() : string {
