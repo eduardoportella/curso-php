21ee8a81dd331820d9fdcef04717d11218dbffa5
Eduardo Portella <eportella70@gmail.com>
2024-02-07 22:41:43 -0300

    aula 050

--- index.php
+++ index.php
@@ -2,8 +2,19 @@
 
 require 'vendor/autoload.php';
 
-echo SITE_NOME;
+// Não importa se já vem formatado ou não
+$document = new \Bissolli\ValidadorCpfCnpj\CPF('123.456.789-09');
 
-echo sistema\Nucleo\Helpers::saudacao();
+// Verifica se é um número válido de CPF
+// Retorna true/false
+var_dump($document->isValid());
+
+// Retorna o número de CPF formatado (###.###.###-##)
+// ou false caso não seja um número válido
+$document->format();
+
+// Retorna o número de sem formatação alguma
+// ou false caso não seja um número válido
+$document->getValue();
 
 ?>
